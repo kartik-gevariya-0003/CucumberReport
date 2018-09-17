53be8534759feea55a434f5ddc64cbd7795ac976
Kartik Gevariya <kartik.gevariya0003@gmail.com>
2018-09-17 08:14:49 +0530

    Pushed latest code.

---
+++ src/main/java/com/report/Test.java
@@ -0,0 +1,8 @@
+package com.report;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+        ReportGenerator.generateCoverageOverviewReport("/Users/kartikgevariya/Extra/CucumberReport1/src/main/java/com/report/cucumber.json", "target");
+        ;
+    }
+}
